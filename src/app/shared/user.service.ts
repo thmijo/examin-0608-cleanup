b07c399360bffae542faa69076ea599546ae843c
thmijo <thmijomail@gmail.com>
2019-06-08 19:24:06 +0530

    5 files modified

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -43,26 +43,6 @@ export class UserService {
       tap(data => console.log(JSON.stringify(data)))
     );
   }
- /* getUserAttempts(uId:string): Observable<any[]> {
-  //getQuestions(eId:string): Observable<any[]> {
-    //console.log(userId);
-
-  
-    console.log("Logging UId from service "+uId);
-    this.attemptsCollection = this.afs.collection(`users/${uId}/attempts`);
-
-
-    return this.attemptsCollection.snapshotChanges().pipe(
-      map(actions =>
-        actions.map(a => {
-          const data = a.payload.doc.data();
-          data.id = a.payload.doc.id;
-          return { ...data };
-        })
-      ),
-      tap(data => console.log(JSON.stringify(data)))
-    );
-  }*/
 
 createAttempt() {
     const attempt = {
@@ -95,34 +75,4 @@ updateAttemptInUserCollection(uId:string,attemptId:string){
 
 
 }
-/*addUserAttempts(uId:string) {
-    const attempt = {
-      test : "Test attempt 333",
-      time: new Date(),
-      dArray :  ['hello','Bolo','Maylo','Julo'],
-      dMap : { 'Environments' : 'Bro', 'servers':'Sis'}  
-    };
-    console.log("adding atempt for "+uId);
-   //this.attemptsCollection = this.afs.collection(`users);
-      return this.attemptsCollection.doc(uId).collection("attempts").add(attempt);
-}
-  //getQuestion(qId: string) {
-  //  return this.afs.doc(`questions/${qId}`);
-  //} */
-
-/*addUserAttempts2(uId:string, questions : any[]) {
-    const attempt = {
-     // test : "Test attempt 333",
-     // time: new Date(),
-     // dArray :  ['hello','Bolo','Maylo','Julo'],
-     // dMap : { 'Environments' : 'Bro', 'servers':'Sis'} ,
-      attemptDetails :  questions
-    };
-    console.log("adding atempt for "+uId);
-   //this.attemptsCollection = this.afs.collection(`users);
-      return this.attemptsCollection.doc(uId).collection("attempts").add(attempt);
-} */
-  //getQuestion(qId: string) {
-  //  return this.afs.doc(`questions/${qId}`);
-  //}
 }
